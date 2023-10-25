d0739312c74760b3e6e360ac2072b35e118b9f04
nhatvu148 <nhatvu148@gmail.com>
2023-10-25 17:46:14 +0700

    update NvimTmuxNavigate

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -9,3 +9,10 @@ vim.keymap.set(
 )
 
 vim.keymap.set("v", "<leader>1f", vim.lsp.buf.format, bufopts)
+
+vim.keymap.set("n", "<C-h>", "<Cmd>NvimTmuxNavigateLeft<CR>", { silent = true })
+vim.keymap.set("n", "<C-j>", "<Cmd>NvimTmuxNavigateDown<CR>", { silent = true })
+vim.keymap.set("n", "<C-k>", "<Cmd>NvimTmuxNavigateUp<CR>", { silent = true })
+vim.keymap.set("n", "<C-l>", "<Cmd>NvimTmuxNavigateRight<CR>", { silent = true })
+vim.keymap.set("n", "<C-\\>", "<Cmd>NvimTmuxNavigateLastActive<CR>", { silent = true })
+vim.keymap.set("n", "<C-Space>", "<Cmd>NvimTmuxNavigateNavigateNext<CR>", { silent = true })
